f342de1397de9b8c17da3ba69f1ac9a554c88b77
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2022-02-02 11:46:52 -0500

    adding comments for code that was written in phase one

--- Program.cs
+++ Program.cs
@@ -7,13 +7,16 @@ namespace plan_your_heist
     {
         static void Main(string[] args)
         {
+            // Heist Member variables that will store ReadLine() user input
             string name;
             int skillLevel;
             decimal courageFactor;
 
+            // heistTeam will store new heist members that are created by user
             List<HeistMember> heistTeam = new List<HeistMember>(){};
             Console.WriteLine("Plan Your Heist!");
             
+            // Heist Member variables will be set to the input from the user
             Console.WriteLine("Enter a teammember's name: ");
             name = Console.ReadLine();
             Console.WriteLine("Enter their skill level (positive whole number): ");
@@ -21,8 +24,10 @@ namespace plan_your_heist
             Console.WriteLine("Enter their courage factor (decimal between 0.0 and 2.0): ");
             decimal.TryParse(Console.ReadLine(), out courageFactor);
 
+            // Instantiating a new HeistMember "hm" by way of its constructor
             HeistMember hm = new HeistMember(name, skillLevel, courageFactor);
 
+            // User input is displayed for the new HeistMember "hm" (name, skill level, and courage factor)
             Console.WriteLine("----------");
             Console.WriteLine($"{hm.Name} \n-Skill Level: {hm.SkillLevel} \n-Courage Factor: {hm.CourageFactor}");
 
