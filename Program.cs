cdf2c79527527fa8cf460f75efa9792f0ba82fd2
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2022-02-02 10:57:27 -0500

    created initial console application template

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace plan_your_heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
