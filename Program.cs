8619a6dd44684142b14f56b0b8c1092040f0a707
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2022-02-02 11:24:27 -0500

    phase one completed, user prompted to input information, user input used in constructor to create a HeistMember

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace plan_your_heist
 {
@@ -6,7 +7,38 @@ namespace plan_your_heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            int skillLevel;
+            decimal courageFactor;
+
+            List<HeistMember> heistTeam = new List<HeistMember>(){};
+            Console.WriteLine("Plan Your Heist!");
+            
+            Console.WriteLine("Enter a teammember's name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter their skill level (positive whole number): ");
+            skillLevel = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter their courage factor (decimal between 0.0 and 2.0): ");
+            decimal.TryParse(Console.ReadLine(), out courageFactor);
+
+            HeistMember hm = new HeistMember(name, skillLevel, courageFactor);
+
+            Console.WriteLine("----------");
+            Console.WriteLine($"{hm.Name} \n-Skill Level: {hm.SkillLevel} \n-Courage Factor: {hm.CourageFactor}");
+
+        }
+    }
+
+    class HeistMember {
+        public string Name {get; set;}
+        public int SkillLevel {get; set;}
+        public decimal CourageFactor {get; set;}
+
+        public HeistMember(string name, int skillLevel, decimal courageFactor)
+        {
+            this.Name = name;
+            this.SkillLevel = skillLevel;
+            this.CourageFactor = courageFactor;
         }
     }
 }
