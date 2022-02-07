045118885f86115db4d4a43f5645958219b2ddf5
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2022-02-07 11:22:22 -0500

    phase five/six completed for part one of exercise, refactored code to incorporate bank difficulty level user input and trial runs

--- Program.cs
+++ Program.cs
@@ -11,45 +11,60 @@ namespace plan_your_heist
             string name;
             int skillLevel;
             decimal courageFactor;
+            string trialRunResponse;
+            int bankDifficultyLevel = 0;
 
             Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine("----------------");
 
             Team team = new Team();
             Bank bank = new Bank();
-            int bankDifficultyLevel = bank.DifficultyLevel + bank.LuckValue();
 
+            Console.WriteLine("Please enter your bank difficulty level: ");
+            int.TryParse(Console.ReadLine(), out bankDifficultyLevel);
 
             while (true)
             {
-
                 // Heist Member variables will be set to the input from the user
                 Console.WriteLine("Enter a teammember's name: ");
                 name = Console.ReadLine();
                 // If name is an empty string, break and terminate while loop
-                if (String.IsNullOrWhiteSpace(name))
+                if (name != "")
                 {
-                    break;
-                }
-                Console.WriteLine("Enter their skill level (positive whole number): ");
-                int.TryParse(Console.ReadLine(), out skillLevel);
-                Console.WriteLine("Enter their courage factor (decimal between 0.0 and 2.0): ");
-                decimal.TryParse(Console.ReadLine(), out courageFactor);
+                    Console.WriteLine("Enter their skill level (positive whole number): ");
+                    int.TryParse(Console.ReadLine(), out skillLevel);
+                    Console.WriteLine("Enter their courage factor (decimal between 0.0 and 2.0): ");
+                    decimal.TryParse(Console.ReadLine(), out courageFactor);
 
-                //Add each new heist member the user creates, then display the size of team
-                team.AddMember(new HeistMember(name, skillLevel, courageFactor));
-
-                if (team.TeamSkillLevel() >= bankDifficultyLevel)
-                {
-                    Console.WriteLine($"Heist Team Skill Level Total: {team.TeamSkillLevel()}");
-                    Console.WriteLine($"Bank Difficulty Level: {bankDifficultyLevel}");
-                    Console.WriteLine("SUCCESS! You may attempt your heist.");
-                    break;
+                    //Add each new heist member the user creates, then display the size of team
+                    team.AddMember(new HeistMember(name, skillLevel, courageFactor));
                 }
                 else
                 {
-                    Console.WriteLine($"\nHeist Team Skill Level Total: {team.TeamSkillLevel()}");
+                    Console.WriteLine("How many trial runs would you like to try?");
+                    trialRunResponse = Console.ReadLine();
+                    int parsedTrialRunResponse = Int32.Parse(trialRunResponse);
+                    int successes = 0;
+                    int failures = 0;
+
+                    for (int i = 0; i < parsedTrialRunResponse; i++)
+                    {
+
+                        if (team.TeamSkillLevel() + bank.LuckValue() > bankDifficultyLevel)
+                        {
+                            successes++;
+                        }
+                        else
+                        {
+                            failures++;
+                        }
+                    }
+
+                    Console.WriteLine($"Team Skill Level: {team.TeamSkillLevel()}");
                     Console.WriteLine($"Bank Difficulty Level: {bankDifficultyLevel}");
-                    Console.WriteLine("FAILURE! Please add another teammember and retry.");
+                    Console.WriteLine($"Successes: {successes}");
+                    Console.WriteLine($"Failures: {failures}");
+                    break;
                 }
 
             }
